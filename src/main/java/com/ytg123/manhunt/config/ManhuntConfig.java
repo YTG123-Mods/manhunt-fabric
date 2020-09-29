0d0b75cd67eca7dcf6d2ff591a904d8b5190311f
Yonatan Dafna <yonatandafna1@gmail.com>
2020-09-29 14:06:44 +0300

    Cleaner code 🧼

--- src/main/java/com/ytg123/manhunt/config/ManhuntConfig.java
+++ src/main/java/com/ytg123/manhunt/config/ManhuntConfig.java
@@ -5,20 +5,22 @@ import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
 
+import static me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry.Gui;
+
 @Config(name = "manhunt")
 public class ManhuntConfig implements ConfigData {
-    @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
+    @Gui.EnumHandler(option = Gui.EnumHandler.EnumDisplayOption.BUTTON)
     @Comment("Sets the behaviour of the compass mechanic, can be either UPDATE or USE. UPDATE = Automatically update the compass every tick. USE = Use the compass to update it (more like Dream's manhunt).")
-    @ConfigEntry.Gui.Tooltip(count = 3)
+    @Gui.Tooltip(count = 3)
     public Behaviours.Compass compassBehaviour = Behaviours.Compass.USE;
 
-    @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
+    @Gui.EnumHandler(option = Gui.EnumHandler.EnumDisplayOption.BUTTON)
     @Comment("Sets the behaviour of the damage mechanic. Can be either KILL or DAMAGE. KILL = The speedrunner loses when they are killed (like Dream's manhunt). DAMAGE = The speedrunner loses when they take damage (like dream's assassin).")
-    @ConfigEntry.Gui.Tooltip(count = 3)
+    @Gui.Tooltip(count = 3)
     public Behaviours.Damage damageBehaviour = Behaviours.Damage.KILL;
 
     @Comment("Freeze the hunter when looked at, similar to Dream's assassin.")
-    @ConfigEntry.Gui.Tooltip(count = 0)
+    @Gui.Tooltip(count = 0)
     public boolean freeze = false;
 
 //    public String[] disabledDimensions = {"minecraft:the_nether"};
