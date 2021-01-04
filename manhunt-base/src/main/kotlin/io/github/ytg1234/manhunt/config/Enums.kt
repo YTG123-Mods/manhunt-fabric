3271eccff7fea91af32343b4d70c6aa44843d7d5
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-01-04 11:46:47 +0200

    maybe fixed KDoc?

--- manhunt-base/src/main/kotlin/io/github/ytg1234/manhunt/config/Enums.kt
+++ manhunt-base/src/main/kotlin/io/github/ytg1234/manhunt/config/Enums.kt
@@ -3,23 +3,33 @@ package io.github.ytg1234.manhunt.config
 /**
  * Decides the behaviour of the hunters' compass.
  *
- * @property UPDATE Compass Updates automatically every tick.
- * @property USE Hunter has to use compass to update it.
- *
  * @author YTG1234
  */
 enum class Compass {
-    UPDATE, USE
+    /**
+     * Compass Updates automatically every tick.
+     */
+    UPDATE,
+
+    /**
+     * Hunter has to use compass to update it.
+     */
+    USE
 }
 
 /**
- * Decides the "damage mode", when the speedrunner dies.
- *
- * @property DAMAGE The speedrunner dies when they take damage.
- * @property KILL The speedrunner dies when they die, like normal Minecraft.
+ * Decides the "damage mode", aka when the speedrunner dies.
  *
  * @author YTG1234
  */
 enum class Damage {
-    DAMAGE, KILL
+    /**
+     * The speedrunner dies when they take damage.
+     */
+    DAMAGE,
+
+    /**
+     * The speedrunner dies when they die, like normal Minecraft.
+     */
+    KILL
 }
